f35186f4a8267a0d423ff98d519de9978bc16b7a
Adam <adam@Thurloat.local>
2009-07-20 11:04:37 -0300

    Phasing out concept of team, in favor of athletes participating in events

--- bios/admin.py
+++ bios/admin.py
@@ -8,14 +8,21 @@ Copyright (c) 2009 __MyCompanyName__. All rights reserved.
 """
 
 from django.contrib import admin
-from bios.models import Athlete, Team, Country
+from bios.models import Athlete, Country
 
+
+#class TeamMemberInline(admin.TabularInline):
+#	model = TeamMember
 class AthleteInline(admin.TabularInline):
 	model = Athlete
-class BioAdmin(admin.ModelAdmin):
-	inlines = (AthleteInline,)
-	
+#class TeamInline(admin.TabularInline):
+#	model = Team
+#class TeamAdmin(admin.ModelAdmin):
+#	inlines = (TeamMemberInline,)
 
-admin.site.register(Country)
-admin.site.register(Team,BioAdmin)
-admin.site.register(Athlete)
+class AthleteAdmin(admin.ModelAdmin):
+	inlines = (AthleteInline,)
+
+admin.site.register(Country, AthleteAdmin)
+
+#admin.site.register(Team, TeamAdmin)
--- bios/models.py
+++ bios/models.py
@@ -9,10 +9,13 @@ Copyright (c) 2009 __MyCompanyName__. All rights reserved.
 
 from django.db.models import permalink, signals
 from google.appengine.ext import db
-from ragendja.dbutils import cleanup_relations
+from ragendja.dbutils import cleanup_relations, KeyListProperty
+from google.appengine.api import memcache
+from norex.properties import ReferenceListProperty
+from results.models import Event
 
 class Country(db.Model):
-	countryNumber = db.StringProperty()
+	countryNumber = db.IntegerProperty()
 	code = db.StringProperty()
 	name = db.StringProperty()
 	
@@ -22,28 +25,25 @@ class Country(db.Model):
 	@permalink
 	def get_absolute_url(self):
 		return ('bios.views.show_country', (), {'key': self.key})
-
-class Team(db.Model):
-	teamNumber = db.StringProperty()
-	country = db.ReferenceProperty(Country)
-	name = db.StringProperty()
 	
-	def __unicode__(self):
-		return '%s. %s - %s' % (self.teamNumber, self.name, self.country)
-			
-	@permalink
-	def get_absolute_url(self):
-		return ('bios.views.show_team', (), {'key': self.key()})
-
-
+	def put(self):
+		memcache.delete("biosHtml")
+		return super(Country,self).put()
+	def delete(self):
+		memcache.delete("biosHtml")
+		return super(Country,self).delete()
 
 class Athlete(db.Model):
-	athleteNumber = db.StringProperty()
+	athleteNumber = db.IntegerProperty()
 	firstName = db.StringProperty()
 	lastName = db.StringProperty()
-	team = db.ReferenceProperty(Team)
+	birthDate = db.DateProperty()
+	homeTown = db.StringProperty()
+	coach = db.StringProperty()
+	picture = db.BlobProperty()
+	events = KeyListProperty(Event)
 	country = db.ReferenceProperty(Country)
-	
+		
 	def __unicode__(self):
 		return '%s. %s %s, %s' % (self.athleteNumber, self.firstName, self.lastName, self.country)
 	
@@ -54,3 +54,32 @@ class Athlete(db.Model):
 	def get_absolute_url(self):
 		return ('bios.views.show_athlete', (), {'key': self.key()})
 
+########################################################
+#Teams Are Deprecated
+########################################################
+"""
+class Team(db.Model):
+	teamNumber = db.IntegerProperty()
+	country = db.ReferenceProperty(Country, required=True)
+	event = db.ReferenceProperty(Event)
+	members = KeyListProperty(Athlete)
+	name = db.StringProperty()
+	
+	def __unicode__(self):
+		return '%s. %s - %s' % (self.teamNumber, self.name, self.country)
+			
+	@permalink
+	def get_absolute_url(self):
+		return ('bios.views.show_team', (), {'key': self.key()})
+	
+	def put(self):
+		memcache.delete("biosHtml")
+		return super(Team,self).put()
+	def delete(self):
+		memcache.delete("biosHtml")
+		return super(Team,self).delete()
+	
+class TeamMember(db.Model):
+	team = db.ReferenceProperty(Team)
+	athlete = db.ReferenceProperty(Athlete)
+"""
